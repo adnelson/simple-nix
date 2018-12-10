e228d247eb4ab1f4b95d01f7713861d275b11320
Allen Nelson <ithinkican@gmail.com>
2018-12-10 13:14:41 -0600

    start working on virtu parser

---
+++ src/Language/Virtu/Parser.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Language.Virtu.Parser where
+
+import Text.Parsec hiding (many, (<|>), spaces, parse, State, uncons)
+
+import Nix.Common
+import Nix.Parser (Parser, keyword, pIdentifier, schar)
+
+newtype Identifier = Identifier Text
+  deriving (Show, Eq, Generic)
+
+data ImportedValue
+  = Type Identifier
+  | Val Identifier
+  | Class Identifier
+  | Everything
+  deriving (Show, Eq, Generic)
+
+type ImportedValues = [(ImportedValue, Maybe Identifier)]
+newtype ModuleIdentifier = ModuleIdentifier [Identifier]
+  deriving (Show, Eq, Generic)
+
+data ImportClause = ImportClause {
+  fromModule :: ModuleIdentifier,
+  importedValues :: ImportedValues
+  }
+  deriving (Show, Eq, Generic)
+
+data Module = Module {
+  imports :: [ImportClause]
+  }
+  deriving (Show, Eq, Generic)
+
+-- NTS: consider mistakes that a user might make when entering the
+-- code. Where possible, make those mistakes easy to avoid, and
+-- otherwise make the error as obvious as possible.
+
+-- TODO maybe should care whether it starts with a capital or not?
+parseIdentifier :: Parser Identifier
+parseIdentifier = Identifier <$> pIdentifier
+
+parseImportedValue :: Parser ImportedValue
+parseImportedValue = choice [
+  map Type (keyword "type" *> parseIdentifier),
+  map Val (keyword "val" *> parseIdentifier),
+  map Class (keyword "class" *> parseIdentifier),
+  Everything <$ keyword "*"
+  ]
+
+parseModuleIdentifier :: Parser ModuleIdentifier
+parseModuleIdentifier = ModuleIdentifier <$> parseIdentifier `sepBy1` schar '.'
+
+parseAliasedImport :: Parser (ImportedValue, Maybe Identifier)
+parseAliasedImport = do
+  ival <- parseImportedValue
+  alias <- optionMaybe $ keyword "as" *> parseIdentifier
+  pure (ival, alias)
+
+inParens :: Parser a -> Parser a
+inParens = between (schar '(') (schar ')')
+
+inCurlies :: Parser a -> Parser a
+inCurlies = between (schar '{') (schar '}')
+
+parseImportClause :: Parser ImportClause
+parseImportClause = do
+  mod <- parseModuleIdentifier
+  imports <- inParens $ parseAliasedImport `sepEndBy1` schar ','
+  pure $ ImportClause mod imports
+
+parseImports :: Parser [ImportClause]
+parseImports = inCurlies $ parseImportClause `sepEndBy` schar ';'
+
+parseModule :: Parser Module
+parseModule = do
+  keyword "module"
+  inCurlies $ do
+    imports <- keyword "import" *> parseImports
+    pure $ Module imports
--- src/Nix.hs
+++ src/Nix.hs
@@ -2,10 +2,12 @@
 module Nix (
     module Nix.Expr,
     module Nix.Parser,
-    module Nix.LicenseType
+    module Nix.LicenseType,
+    module Language.Virtu.Parser
   ) where
 
 import Nix.Common
 import Nix.Expr
 import Nix.Parser
 import Nix.LicenseType
+import Language.Virtu.Parser
